0654c2f82bdf850ba068b4bea7fdfb65a6ead1a7
Mike Fairhurst <michaelrfairhurst@gmail.com>
2024-02-07 12:52:32 -0800

    Remove unused roast service

--- lib/src/roast/services/roast_service.dart
+++
@@ -1,8 +0,0 @@
-import 'package:behmor_roast/src/roast/models/roast.dart';
-import 'package:flutter/foundation.dart';
-
-class RoastService extends ChangeNotifier {
-  Roast roast;
-
-  RoastService(this.roast);
-}
