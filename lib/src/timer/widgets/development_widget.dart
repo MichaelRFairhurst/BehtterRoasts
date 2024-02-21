e1da1197668ad6282ded25ba114fef09eb79b156
Mike Fairhurst <michaelrfairhurst@gmail.com>
2024-02-21 12:24:24 -0800

    Fix loss of functionality: development time updates live again.
    Previously, development time and roast time used a TickerProvider to
    show the time every frame. This is overkill for roast time which only
    needs to rebuild once per second, so a recent commit refactored this to
    user `Timer.seconds` to rebuild once per second.
    
    However, development time also only rebuilt once per second. This isn't
    ideal, especially if showing development time with a 0.1% granularity.
    Even at 1% granularity, a percentage of development is not going to
    increase at the same moment the seconds change.
    
    Fix this by pulling development into its own widget that uses a
    TickerProvider.

---
+++ lib/src/timer/widgets/development_widget.dart
@@ -0,0 +1,67 @@
+
+import 'package:behmor_roast/src/roast/models/phase_log.dart';
+import 'package:behmor_roast/src/roast/providers.dart';
+import 'package:behmor_roast/src/timer/providers.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class DevelopmentWidget extends ConsumerStatefulWidget {
+  const DevelopmentWidget({super.key});
+
+  @override
+  DevelopmentWidgetState createState() => DevelopmentWidgetState();
+}
+
+class DevelopmentWidgetState extends ConsumerState<DevelopmentWidget> with SingleTickerProviderStateMixin {
+
+  Duration? time;
+  late Ticker ticker;
+
+  @override
+  void initState() {
+	super.initState();
+	ticker = createTicker((_) {
+	  setState(() {
+		time = ref.read(timerServiceProvider).elapsed()!;
+	  });
+	})..start();
+  }
+
+  @override
+  void dispose() {
+	super.dispose();
+	ticker.stop();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+	return Row(
+	  mainAxisSize: MainAxisSize.min,
+	  children: developmentTimeParts(),
+	);
+  }
+
+  List<Widget> developmentTimeParts() {
+    final phases = ref.watch(phaseLogsProvider);
+    final roast = ref.watch(roastProvider);
+	if (!phases.any((phase) => phase.phase == Phase.dryEnd)) {
+      return [const Text('Waiting for dry end.')];
+	}
+
+    final firstCracks = phases.where((phase) => phase.phase == Phase.crack);
+    if (firstCracks.isEmpty) {
+      return [const Text('Waiting for first crack.')];
+    }
+
+    final firstCrackEnd = firstCracks.last.time;
+    final development = (time! - firstCrackEnd).inMilliseconds / time!.inMilliseconds;
+
+    final develFmt = (development * 100).toStringAsFixed(1);
+    final targetFmt = (roast!.config.targetDevelopment * 100).toStringAsFixed(1);
+
+    return [
+      Text('$develFmt/$targetFmt% development'),
+    ];
+  }
+}
--- lib/src/timer/widgets/time_widget.dart
+++ lib/src/timer/widgets/time_widget.dart
@@ -1,7 +1,5 @@
-import 'package:behmor_roast/src/roast/models/phase_log.dart';
-import 'package:behmor_roast/src/roast/providers.dart';
 import 'package:behmor_roast/src/timer/providers.dart';
-import 'package:behmor_roast/src/timer/services/timer_service.dart';
+import 'package:behmor_roast/src/timer/widgets/development_widget.dart';
 import 'package:behmor_roast/src/timer/widgets/timestamp_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -20,37 +18,12 @@ class TimeWidget extends ConsumerWidget {
     return Row(
 	  children: [
         const SizedBox(width: 20),
-        ...developmentTimeParts(ref),
+        const DevelopmentWidget(),
         const Spacer(),
 	    const Text('Roast time: '),
 	    TimestampWidget.twitter(time),
 	    const SizedBox(width: 20),
 	  ],
 	);
-  }
-
-  List<Widget> developmentTimeParts(WidgetRef ref) {
-    final phases = ref.watch(phaseLogsProvider);
-    final roast = ref.watch(roastProvider);
-    final tService = ref.watch(timerServiceProvider);
-	if (!phases.any((phase) => phase.phase == Phase.dryEnd)) {
-      return [const Text('Waiting for dry end.')];
-	}
-
-    final firstCracks = phases.where((phase) => phase.phase == Phase.crack);
-    if (firstCracks.isEmpty) {
-      return [const Text('Waiting for first crack.')];
-    }
-
-    final firstCrackEnd = firstCracks.last.time;
-    final now = tService.elapsed()!;
-    final development = (now - firstCrackEnd).inMilliseconds / now.inMilliseconds;
-
-    final develFmt = (development * 100).toStringAsFixed(1);
-    final targetFmt = (roast!.config.targetDevelopment * 100).toStringAsFixed(1);
-
-    return [
-      Text('$develFmt/$targetFmt% development'),
-    ];
   }
 }
