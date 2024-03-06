709d8d90648bde4e295dfbe06e4269f18aeccdbb
Mike Fairhurst <michaelrfairhurst@gmail.com>
2024-03-06 13:55:40 -0800

    Fix roast tips for preheat workflow.

--- lib/src/timer/services/tips_service.dart
+++ lib/src/timer/services/tips_service.dart
@@ -1,28 +1,35 @@
 import 'package:behmor_roast/src/roast/models/roast_log.dart';
+import 'package:behmor_roast/src/timer/models/roast_timeline.dart';
 
 class TipsService {
-  Set<String> getTips(List<RoastLog> logs, bool running) {
+  Set<String> getTips(RoastTimeline timeline, List<RoastLog> logs) {
     final results = <String>{};
-    if (!running && logs.isEmpty) {
-      results.add('Preheat the roaster to 180-220 for a faster, hotter roast.');
+    if (timeline.roastState == RoastState.ready) {
       results.add('Use the 1lb setting on all batch sizes to maximize available'
           ' time.');
       return results;
     }
-    if (!logs.any((log) => log.control != null)) {
+
+    final roasting = timeline.roastState == RoastState.roasting;
+
+    if (roasting && !logs.any((log) => log.control != null)) {
       results.add('Press P1-P5 to enter manual roast mode.');
     }
-    if (!logs.any((log) => log.phase != null)) {
+
+    if (roasting && timeline.dryEnd == null) {
       results.add('The dry phase is over when the beans are yellow and have a'
           ' bread-like (rather than hay-like) aroma.');
     }
-    if (running &&
-        logs.any((log) => log.phase == RoastPhase.firstCrackStart) &&
-        !logs.any((log) => log.phase == RoastPhase.firstCrackEnd)) {
+
+    if (roasting &&
+        timeline.firstCrackStart == timeline.firstCrackEnd &&
+        timeline.firstCrackStart != null &&
+        timeline.secondCrackStart == null) {
       results.add("Keep pressing 'Log Crack' while you hear pops, in order to"
           ' track the end of the First Crack phase.');
     }
-    if (logs.any((log) => log.phase == RoastPhase.done)) {
+
+    if (timeline.roastState == RoastState.done) {
       results.add('Remove the drum from the roaster to cool the beans more'
           ' quickly.');
     }
