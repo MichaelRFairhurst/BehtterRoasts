5a53ee73f9e73cbdf9f62a2ffb3c86f77d099151
Mike Fairhurst <michaelrfairhurst@gmail.com>
2024-07-11 12:32:12 -0700

    Use code configuration for ios google sign in

--- lib/src/sign_in/services/sign_in_service.dart
+++ lib/src/sign_in/services/sign_in_service.dart
@@ -1,9 +1,16 @@
+import 'dart:io' show Platform;
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
 class SignInService {
   Future<void> signIn() async {
-    final googleUser = await GoogleSignIn().signIn();
+    final googleSignIn = GoogleSignIn(
+      clientId: Platform.isIOS
+          ? '476682855230-povgmevit3agcfchqgthgqafp38igtep.apps.googleusercontent.com'
+          : null,
+    );
+    final googleUser = await googleSignIn.signIn();
     final googleAuth = await googleUser?.authentication;
     final credential = GoogleAuthProvider.credential(
       accessToken: googleAuth?.accessToken,
