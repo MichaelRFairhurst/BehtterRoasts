793dc05f950d8931fcb9f7717c54af1c22292f57
Mike Fairhurst <michaelrfairhurst@gmail.com>
2024-07-11 14:33:07 -0700

    Don't even import wakelock, see if this fixes ios

--- lib/src/timer/services/wakelock_service.dart
+++ lib/src/timer/services/wakelock_service.dart
@@ -1,29 +1,20 @@
-import 'dart:io';
-
-import 'package:wakelock_plus/wakelock_plus.dart';
+// TODO: Get working on iOS and re-enable
+//import 'package:wakelock_plus/wakelock_plus.dart';
 
 class WakelockService {
   final _clients = <String>{};
 
   void requestOn(String id) {
-    if (Platform.isIOS) {
-      // TODO: Get working on iOS and re-enable
-      return;
-    }
-
-    _clients.add(id);
-    WakelockPlus.enable();
+    // TODO: Get working on iOS and re-enable
+    //_clients.add(id);
+    //WakelockPlus.enable();
   }
 
   void requestOff(String id) {
-    if (Platform.isIOS) {
-      // TODO: Get working on iOS and re-enable
-      return;
-    }
-
-    _clients.remove(id);
-    if (_clients.isEmpty) {
-      WakelockPlus.disable();
-    }
+    // TODO: Get working on iOS and re-enable
+    //_clients.remove(id);
+    //if (_clients.isEmpty) {
+    //  WakelockPlus.disable();
+    //}
   }
 }
