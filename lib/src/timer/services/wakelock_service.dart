15036c225037e0c9cb9c5440a3f05717922849ad
Mike Fairhurst <michaelrfairhurst@gmail.com>
2024-07-12 14:49:40 -0700

    Revert "Disable wakelock on ios"
    This reverts commit 5b68ac850ac3805578bdb35160558bc63d9e3812.

--- lib/src/timer/services/wakelock_service.dart
+++ lib/src/timer/services/wakelock_service.dart
@@ -1,26 +1,14 @@
-import 'dart:io';
-
 import 'package:wakelock_plus/wakelock_plus.dart';
 
 class WakelockService {
   final _clients = <String>{};
 
   void requestOn(String id) {
-    if (Platform.isIOS) {
-      // TODO: Get working on iOS and re-enable
-      return;
-    }
-
     _clients.add(id);
     WakelockPlus.enable();
   }
 
   void requestOff(String id) {
-    if (Platform.isIOS) {
-      // TODO: Get working on iOS and re-enable
-      return;
-    }
-
     _clients.remove(id);
     if (_clients.isEmpty) {
       WakelockPlus.disable();
